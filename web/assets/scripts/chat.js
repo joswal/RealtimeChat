41140d22f49bfa102954a7d2388a4471aa61d251
Joswal <princejoswal@gmail.com>
2019-06-15 10:12:19 +0100

    edited chat.js

--- web/assets/scripts/chat.js
+++ web/assets/scripts/chat.js
@@ -10,35 +10,36 @@ var file, textInput, firebase, timer;
 //on completion of page load
 $(document).ready(function () {
 
-// declaration of function variables to select the needed html elements
+    // declaration of function variables to select the needed html elements
     var messagesList = $('#messages'),
-            textInput = $('#text'),
-            sendButton = $('#send'),
-            googleLogin = $('#google'),
-            anonLogin = $('#anonLogin'),
-            logout = $('#logout'),
-            usernameElm = $('#username'),
-            username = "notLoggedUser",
-            uploader = $('#uploader'),
-            config;
-            
+        textInput = $('#text'),
+        sendButton = $('#send'),
+        googleLogin = $('#google'),
+        anonLogin = $('#anonLogin'),
+        logout = $('#logout'),
+        usernameElm = $('#username'),
+        username = "notLoggedUser",
+        uploader = $('#uploader'),
+        config;
+
     // configuration of the firebase credentials for use in the app
     config = {
-        apiKey: "AIzaSyBMjfKCUSgwCDxFcwTh9vv010fIMOjfAVo",
+        //replace "your API key" with your API key from firebase in the line below here
+        apiKey: "your API key",
         authDomain: "chatapp-a40df.firebaseapp.com",
         databaseURL: "https://chatapp-a40df.firebaseio.com",
         projectId: "chatapp-a40df",
         storageBucket: "chatapp-a40df.appspot.com",
         messagingSenderId: "470436860368"
     };
-//initialization of the firebase data into our app
+    //initialization of the firebase data into our app
     var app = firebase.initializeApp(config); //initialize our app using the firebase config settings
     var database = app.database(); // initialize our app database as the firebase database
     var auth = app.auth(); //initialize our app authentication as the firebase authentication
     var storage = app.storage(); //initialize our app storage as the firebase storage
     var databaseRef = database.ref().child('chat'); //create a child called chat for the database
 
-//function to send msg
+    //function to send msg
     sendButton.click(function () {
         var chat = {
             name: username,
@@ -49,42 +50,55 @@ $(document).ready(function () {
         textInput.value = '';
         timer = getCurrentTime();
     });
-    
+
     //function to listen to message
     databaseRef.on('child_added', function (snapshot) {
         var chat = snapshot.val();
         addMessage(chat);
     });
-    
+
     //function to place the listened message into containers and seperate them
     function addMessage(chat) {
-        var li = $('<li />', {class: "clearfix"});
-        var msgData = $("<div />", {class: "message-data"}).prependTo(li);
-        var msgName = $("<span />", {text: chat.name, class: "message-data-name"}).prependTo(msgData);
-        var msgIcon = $("<i />", {class: "message-data-time"}).prependTo(msgName);
-        var msgTime = $("<span />", {text: chat.time, class: "message-data-time"}).appendTo(msgData);
-        if ( textInput===chat.message){
-                var msgDetail = $("<div />", {text: chat.message, class: "message my-message align-right"}).appendTo(li);
-            }
-            else {
-                msgDetail = $("<div />", {text: chat.message, class: "message other-message align-left"}).appendTo(li);
-            }
-        
+        var li = $('<li />', {
+            class: "clearfix"
+        });
+        var msgData = $("<div />", {
+            class: "message-data"
+        }).prependTo(li);
+        var msgName = $("<span />", {
+            text: chat.name,
+            class: "message-data-name"
+        }).prependTo(msgData);
+        var msgIcon = $("<i />", {
+            class: "message-data-time"
+        }).prependTo(msgName);
+        var msgTime = $("<span />", {
+            text: chat.time,
+            class: "message-data-time"
+        }).appendTo(msgData);
+        if (textInput === chat.message) {
+            var msgDetail = $("<div />", {
+                text: chat.message,
+                class: "message my-message align-right"
+            }).appendTo(li);
+        } else {
+            msgDetail = $("<div />", {
+                text: chat.message,
+                class: "message other-message align-left"
+            }).appendTo(li);
+        }
+
         li.appendTo(messagesList);
     }
-    
-    function getCurrentTime(){
+
+    function getCurrentTime() {
         var d = new Date();
         /*var month =d.getMonth()+1;
         var day = d.getDate();*/
-        var hour= d.getHours();
-        var mins= d.getMinutes();
-        var time= hour + mins;
+        var hour = d.getHours();
+        var mins = d.getMinutes();
+        var time = hour + mins;
         return time;
     }
 
 });
-
-
-
-
